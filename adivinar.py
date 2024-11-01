0b449996475af3c3913ad6ccbd2ab7d2d966bb9d
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-11-01 04:38:45 -0600

    Update adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -1,12 +1,5 @@
 import random
 
+# Genera un número aleatorio entre 1 y 100
 numA = random.randint(1, 100)
-numero = -1
-while numero != numA:
-    numero = int(input("Introduce un número: "))
-    if numero > numA:
-        print("Te pasaste")
-    elif numero < numA:
-        print("Te quedaste corto")
 
-print(f"Felicidades, El numero es: {numA}")
