fd295e024ee77f909b644547a76e0618ebd0f005
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-11-03 22:25:02 -0600

    Update drawHeart.py

--- drawHeart.py
+++ drawHeart.py
@@ -4,3 +4,8 @@ from turtle import *  # Importa todas las funciones de la biblioteca turtle para
 def hearta(k):
     # Calcula la coordenada x del corazón usando una fórmula basada en seno.
     return 15 * math.sin(k)**3
+    
+def heartb(k):
+    # Calcula la coordenada y del corazón usando una fórmula basada en coseno.
+    return 12 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
+
