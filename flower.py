6de0a25a6dc70a939bff2da47413dce6ec2523ee
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-11-05 10:34:33 -0600

    Update flower.py

--- flower.py
+++ flower.py
@@ -1,3 +1,18 @@
 from turtle import *
 import turtle
 import math
+
+tracer(2)
+h = 0
+bgcolor('black')
+pensize(2)
+color('yellow')
+
+for i in range(195):
+    h += 0.006
+    lt(179)
+    backward(i * 0.1)
+    circle(i * 0.3, 120)
+    rt(14)
+    forward(i * 0.1)
+    circle(i * 0.3, 120)
