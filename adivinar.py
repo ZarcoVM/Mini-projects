1969c371c4272ae84aafd92d17ea11f57c16b7ea
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-11-26 09:20:03 -0600

    Update adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -9,6 +9,7 @@ numA = random.randint(1, 100)
 numero = -1
 intentos = 0
 
+
 # Bucle que continúa hasta que el usuario adivine el número
 while numero != numA:
     # Solicita al usuario que introduzca un número
