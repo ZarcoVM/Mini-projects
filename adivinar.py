cd3b0841587d43c0db032f7a4d03a9709f22058e
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-12-29 22:31:07 -0600

    Actualizar adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -6,7 +6,7 @@ numA = random.randint(1, 100)
 # Inicializa la variable para almacenar el número ingresado por el usuario
 # y el contador de intentos
 numero = -1
-intentos = 0
+intentos = 0 
 
 # Bucle que continúa hasta que el usuario adivine el número 
 while numero != numA:
