31bd80a749546e3ec9d06cef60b6d9a8a5bd3370
Angel Zarco <91713291+ZarcoDev4@users.noreply.github.com>
2025-02-10 22:55:28 -0600

    Actualizar adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -4,7 +4,8 @@ import random
 
 numA = random.randint(1, 100)
 
-# Inicializa la variable para almacenar el número ingresado por el usuario
+# Inicializa la variable para almacenar el
+ número ingresado por el usuario
 # y el contador de intentos
 numero = -1
 intentos = 0 
