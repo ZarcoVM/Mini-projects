57a75f7eee03639b84a05cd00dd44febf62eab41
Angel Zarco <91713291+ZarcoDev4@users.noreply.github.com>
2025-02-13 20:57:07 -0600

    Update adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -3,6 +3,7 @@ import random
 # Genera un número aleatorio entre 1 y 100
 
 numA = random.randint(1, 100)
+
 # Inicializa la variable para almacenar el
  número ingresado por el usuario
 # y el contador de intentos
